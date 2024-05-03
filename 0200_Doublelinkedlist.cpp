dba238bb65992c27c763187e33451b3a3b0ef9e5
Refinaldii <refinaldi9786@gmail.com>
2024-05-03 14:14:49 +0700

    membuat conditional statement

--- 0200_Doublelinkedlist.cpp
+++ 0200_Doublelinkedlist.cpp
@@ -20,7 +20,12 @@ void addNode(){
 	cin >> newNode->name;
 
 	if (START == NULL || newNode->noMhs <= START->noMhs) {
+		if (START != NULL && newNode->noMhs == START->noMhs) {
+			cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+			return;
+		}
 
+			
 	}
 }
 int main()
